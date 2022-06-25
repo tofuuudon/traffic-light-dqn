695114e14de27e7f020af601052bc42d1e6a8ff9
Mikey Lau <mikey.lau@traininblocks.com>
2022-06-25 17:33:14 +0100

    chore: Create project

---
+++ src/main.py
@@ -0,0 +1,28 @@
+"""Instantiates SUMO with pre-defined network and config for DQN."""
+
+import os
+import sys
+
+# pylint disable: import-error
+
+import traci
+from sumolib import checkBinary
+
+# Checks for SUMO_HOME enviroment
+if "SUMO_HOME" in os.environ:
+    tools = os.path.join(os.environ["SUMO_HOME"], "tools")
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+sumoBinary = checkBinary("sumo-gui")
+sumoCmd = [sumoBinary, "-c", "data/london/osm.sumocfg"]
+
+traci.start(sumoCmd)
+STEP = 0
+
+while STEP <= 3600:
+    traci.simulationStep()
+    STEP += 1
+
+traci.close()
