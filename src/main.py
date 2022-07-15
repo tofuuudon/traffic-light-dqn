bf59efe7779c03147064010666fd05e5bb39e0e2
Mikey Lau <mikey.lau@traininblocks.com>
2022-07-15 22:13:58 +0100

    feat(main): Dynamically gets all 16-states ids

--- src/main.py
+++ src/main.py
@@ -18,11 +18,22 @@ else:
 sumoBinary = checkBinary("sumo")
 sumoCmd = [sumoBinary, "-W", "-c", "data/train-network/osm.sumocfg"]
 
-traci.start(sumoCmd)
 STEP = 0
+N_STEP = 3600
 
-while STEP <= 3600:
+traci.start(sumoCmd)
+
+# Gets all 4-way intersections with 16 states
+TLS_IDS: list[int] = [
+    id
+    for id in traci.trafficlight.getIDList()
+    if len(traci.trafficlight.getRedYellowGreenState(id)) == 16
+]
+
+# Runtime
+while STEP <= N_STEP:
     traci.simulationStep()
+
     STEP += 1
 
 traci.close()
